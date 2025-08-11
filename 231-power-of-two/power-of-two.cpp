c3bd54104c8ca59dd6fa5732980f266ec12d009a
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-11 23:25:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (82.91%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0)return false;
-        if (n==1)return true;
-        if(n%2==1)return false;
-        return isPowerOfTwo(n/2);
+        if(n<0)return false;
+      int t=__builtin_popcount(n);
+      return t==1;
     }
 };
