6cc644c00bebcdd18e7c444b248c6f00713876c6
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-14 21:30:53 +0530

    Time: 456 ms (39.31%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,13 +1,14 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-  DECLARE offset_val INT;
-  SET offset_val = N - 1;
-
+declare m int;
+set m = N-1;
   RETURN (
-     select  distinct salary
-     from employee
-     order by salary desc
-     limit 1 OFFSET offset_val
+    select distinct salary 
+    from employee
+    order by salary desc
+    limit 1  offset m
+
+     
 
   );
 END
