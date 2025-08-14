9bd1fbd0014ff495cb4146d3348114abf5b5175f
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-14 17:08:00 +0530

    Time: 759 ms (44.64%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,7 +1,7 @@
 
        with cte as( 
         select de.name as department ,ee.name as employee,ee.salary,
-        max(salary) over(partition by ee.departmentid )as maxsalary
+        dense_rank() over(partition by ee.departmentid order by ee.salary desc  )as maxsalary
         from employee ee
         join department de
         on ee.departmentid=de.id
@@ -9,7 +9,7 @@
 
          select Department,Employee,salary as Salary
        from cte
-       where maxsalary=salary
+       where maxsalary=1
 
 
 
