3dbe29ba7ccc3d0651cee06b569fb28134432e40
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-20 19:36:02 +0530

    Time: 1101 ms (5.08%) | Memory: 404.8 MB (5.08%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-int maxi=0;
-void func(string s,unordered_set<string>ans,int index){
-    if(index==s.size()){
-        maxi=max(maxi,(int)ans.size());
+
+void func(string s,unordered_set<string>ans,int i,int  &maxicount,int currcount){
+    if(i==s.size()){
+        maxicount=max(maxicount,currcount);
         return ;
 
     }
     string curr;
-    for(int i=index;i<s.size();i++){
-        curr+=s[i];
+    for(int j=i;j<s.size();j++){
+        curr+=s[j];
       if(ans.find(curr)==ans.end()){
         ans.insert(curr);
-        func(s,ans,i+1);
+        func(s,ans,j+1,maxicount,currcount+1);
         ans.erase(curr);
       }
     }
 }
     int maxUniqueSplit(string s) {
         unordered_set<string>ans;
-        func(s,ans,0);
-        return maxi;
+        int maxicount=0;
+        int currcount=0;
+        func(s,ans,0,maxicount,currcount);
+        return maxicount;
         
     }
 };
