7c47b4bce4ee8e809e8b2e7e186571326a7c2956
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-21 14:23:43 +0530

    Time: 39 ms (5.95%) | Memory: 17.9 MB (5.08%) - LeetSync

---
+++ 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+   bool func(string s,int k){
+
+    int count1=0;
+    int count2=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='0')count1++;
+        else{
+            count2++;
+        }
+    }
+    return (count1<=k || count2<=k);
+   }
+    int countKConstraintSubstrings(string s, int k) {
+        int count=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            string ans="";
+            for(int j=i;j<n;j++){
+                ans+=s[j];
+                if(func(ans,k)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
