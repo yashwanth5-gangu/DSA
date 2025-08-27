d3b0ec3ca23de68d580c03799325e5d3ff997908
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-27 15:27:03 +0530

    Time: 50 ms (36.80%) | Memory: 104.7 MB (44.16%) - LeetSync

---
+++ 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long minimumRemoval(vector<int>& beans) {
+        sort(beans.begin(),beans.end());
+        long long sum=accumulate(beans.begin(),beans.end(),0ll);
+        long long maxarea=0;
+        int n=beans.size();
+        for(int i=0;i<n;i++){
+            maxarea=max(maxarea,(long long )beans[i] *(n-i)*1ll);
+
+        }
+        return sum-maxarea;
+    }
+};
