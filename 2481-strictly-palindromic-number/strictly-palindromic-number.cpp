574a10596f88e11c3e0f3c069a5486297f61ac92
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-29 14:21:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (14.14%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
@@ -1,7 +1,33 @@
 class Solution {
 public:
+string get(int n,int b){
+    string y;
+    while(n){
+        if(n%b!=0){
+            int m=n%b;
+             y+=m+'0';
+             n=n/b;
+        }
+        else{
+            y+='0';
+            n=n/b;
+        }
+    }
+    return y;
+}
+bool func(string y){
+    string t=y;
+    reverse(t.begin(),t.end());
+    return t==y;
+}
     bool isStrictlyPalindromic(int n) {
-        return false;
-        
+       for(int i=2;i<=n-2;i++){
+        string s=get(n,i);
+        if(!func(s)){
+             return false;
+            
+        }
+       }
+       return true;
     }
 };
