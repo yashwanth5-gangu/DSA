08dcd5eedd3f0ee85c2210a4e8468725932f4616
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-08-30 14:44:41 +0530

    Time: 3 ms (62.80%) | Memory: 23 MB (66.59%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         unordered_set<char>st;
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board.size();j++){
                 if(board[i][j]=='.')continue;
             if(st.find(board[i][j])!=st.end()){
                 return false;
@@ -14,10 +14,10 @@ public:
         }
         st.clear();
         }
-        unordered_set<char>st1;
-         for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[j][i]=='.')continue;
+         unordered_set<int>st1;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board.size();j++){
+                  if(board[j][i]=='.')continue;
             if(st1.find(board[j][i])!=st1.end()){
                 return false;
             }
@@ -26,25 +26,26 @@ public:
             }
         }
         st1.clear();
-
-         }
-        
-for (int boxRow = 0; boxRow < 9; boxRow += 3) {
-    for (int boxCol = 0; boxCol < 9; boxCol += 3) {
-        unordered_set<char> boxSet;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                char c = board[boxRow + i][boxCol + j];
-                if (c == '.') continue;
-                if (boxSet.find(c) != boxSet.end()) {
-                    return false;
+        }
+     
+      
+        for(int row=0;row<board.size();row+=3){
+            for(int col=0;col<board.size();col+=3){
+                  unordered_set<char>st3;
+                for(int i=0;i<3;i++){
+                    for(int j=0;j<3;j++){
+                          if(board[row+i][col+j]=='.')continue;
+                        if(st3.find(board[row+i][col+j])!=st3.end()){
+                            return false;
+                        }
+                        else{
+                            st3.insert(board[row+i][col+j]);
+                        }
+                    }
                 }
-                boxSet.insert(c);
+                st3.clear();
             }
         }
-    }
-}
-
-        return true;
+           return true;
     }
 };
