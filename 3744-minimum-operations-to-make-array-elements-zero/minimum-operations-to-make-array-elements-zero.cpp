f872bbb15c7c3b981fff8b710cc33fdd73303146
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-09-06 21:26:34 +0530

    Time: 11 ms (82.61%) | Memory: 183.3 MB (64.49%) - LeetSync

---
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+long long solve(int l,int r){
+    long long L=1;
+    long long S=1;
+    long long steps=0;
+    while(L<=r){
+        long long R=4*L-1;
+        long long start=max(L,(long long)l);
+        long long end=min(R,(long long )r);
+        if(start<=end){
+            steps+=(end-start+1)*S;
+        }
+        S++;
+        L=L*4;
+    }
+    return steps;
+}
+    long long minOperations(vector<vector<int>>& queries) {
+        long long result=0;
+        for(auto &it :queries){
+            int l=it[0];
+            int r=it[1];
+            long long steps=solve(l,r);
+
+            result+=(steps+1)/2;
+        }
+        return result;
+        
+    }
+};
