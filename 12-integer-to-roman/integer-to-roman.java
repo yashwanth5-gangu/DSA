db68b1df68b75c9a4499b7850a21e34804701eca
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-09-07 15:24:53 +0530

    Time: 4 ms (35.11%) | Memory: 44.8 MB (22.07%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -2,13 +2,13 @@ class Solution {
     public String intToRoman(int num) {
         List<Integer> li=Arrays.asList(1000,900,500,400,100,90,50,40,10,9,5,4,1);
         List<String> str=Arrays.asList("M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I");
-        String ans="";
+        StringBuilder ans= new StringBuilder();
         for(int i=0;i<13;i++){
             while(num>=li.get(i)){
-                ans+=str.get(i);
+                ans.append(str.get(i));
                 num=num-li.get(i);
             }
         }
-        return ans;
+        return ans.toString();
     }
 }
