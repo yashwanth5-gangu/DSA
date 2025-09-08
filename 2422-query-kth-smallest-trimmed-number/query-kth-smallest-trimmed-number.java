e083f5a0e8c856c4672c7c925376fd62a62c02da
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-09-08 21:24:44 +0530

    Time: 240 ms (49.77%) | Memory: 45.5 MB (70.23%) - LeetSync

---
+++ 2422-query-kth-smallest-trimmed-number/query-kth-smallest-trimmed-number.java
@@ -0,0 +1,36 @@
+class Solution {
+    class Pair{
+        String ans;
+        int index;
+        Pair(String s,int i){
+            this.ans=s;
+            this.index=i;
+        }
+    }
+    public int[] smallestTrimmedNumbers(String[] nums, int[][] queries) {
+        List<Integer> li=new ArrayList<>();
+        for(int []it :queries){
+            int t=it[0];
+            int y=it[1];
+             List<Pair> new1=new ArrayList<>();
+             for(int i=0;i<nums.length;i++){
+                String num=nums[i];
+             new1.add(new Pair(num.substring(num.length()-y),i));
+             }
+             Collections.sort(new1,(a,b)->{
+                int cmp = a.ans.compareTo(b.ans);  
+            if (cmp == 0) return a.index - b.index;    
+             return cmp;
+             });
+            li.add(new1.get(t-1).index);
+             
+             
+        }
+        int t=queries.length;
+        int [] arr=new int[li.size()];
+        for(int i=0;i<t;i++){
+            arr[i]=li.get(i);
+        }
+        return arr;
+    }
+}
