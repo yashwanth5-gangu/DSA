096590e52a5d8f9511f86e71f919b3d98928241a
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-09-11 00:03:30 +0530

    Time: 1 ms (40.26%) | Memory: 44 MB (13.11%) - LeetSync

--- 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.java
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.java
@@ -1,9 +1,13 @@
 class Solution {
     public int countPrefixes(String[] words, String s) {
+        Set<String>st=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            String s1=s.substring(0,i+1);
+            st.add(s1);
+        }
         int count=0;
-        for(int i=0;i<words.length;i++){
-           int len=words[i].length();
-           if(len <= s.length()&& s.substring(0,len).equals(words[i]))count++;
+        for(String str:words){
+            if(st.contains(str))count++;
         }
         return count;
     }
