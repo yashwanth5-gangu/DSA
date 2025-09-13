f0ddc56ffcfc61751936962056538fbd5069ed9f
yashwanth5-gangu <yashwanthreddygangu@gmail.com>
2025-09-13 23:41:40 +0530

    Time: 4 ms (52.30%) | Memory: 17 MB (43.10%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
+static bool compare(string &a,string &b){
+    return a+b>b+a;
+}
     string largestNumber(vector<int>& nums) {
 
         vector<string>ans;
         for(auto &it :nums){
             ans.push_back(to_string(it));
         }
-        for(int i=0;i<ans.size()-1;i++){
-            for(int j=0;j<ans.size()-i-1;j++){
-                string first=ans[j]+ans[j+1];
-                string second=ans[j+1]+ans[j];
-                if(second>first){
-                    swap(ans[j],ans[j+1]);
-                }
-            }
-        }
+       sort(ans.begin(),ans.end(),compare);
         string ans1;
         for(int i=0;i<ans.size();i++){
             ans1+=ans[i];
